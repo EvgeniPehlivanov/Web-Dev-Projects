60ec04a18ae44ac6426a3c8532e853bac121d8c4
Evgeni Pehlivanov <evgeni.k.pehlivanov@gmail.com>
2023-04-11 05:32:54 +0300

    Updated Multiple Things
    Portfolio-Contact:
    - Updated "Contact" so it features my e-mail and telephone number, instead of just being a form which does nothing
    
    Portfolio-About:
    - Changed text above JavaScript icon from "JavaScript (Mastering)" to just "JavaScript"
    
    Portfolio-Projects:
    - The links to my projects have always been off-centered which I tried to fix by adding a few small things which didn't really fix the issue, but it makes it less noticable
    
    "Expense-Calculator" Project:
    - Changed the style of the input box and button
    - Fixed the code breaking when nothing was put into the input box (turns out instead of using || for the if, I used &&. Rookie mistake!)

--- Portfolio site/expense calculator/app.js
+++ Portfolio site/expense calculator/app.js
@@ -1,9 +1,8 @@
-const input = document.getElementById("input")
-const profit = document.getElementById("profit")
-const cost = document.getElementById("cost")
+const input = document.querySelector("#input")
+const profit = document.querySelector("#profit")
+const cost = document.querySelector("#cost")
 
 const posNums = [];
-const negNums = [];
 
 input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") addItem();
@@ -14,22 +13,20 @@ function calculate() {
     posNums.push(parseInt(input.value));
 
     let result = (posNums.reduce((a, b) => { return a + b; })).toFixed(2);
-    if (result >= 0) { 
+    if (result >= 0) {
         profit.innerHTML = '<h4>' + '+' + result + '</h4>';
-        cost.innerHTML = '<h4>' + '-' +0.00 + '</h4>'
-     }
+        cost.innerHTML = '<h4>' + '-' + 0.00.toFixed(2); + '</h4>'
+    }
     else if (result <= 0) {
         cost.innerHTML = '<h4>' + result + '</h4>'
-        profit.innerHTML = '<h4>' + '+' +0.00 + '</h4>';
+        profit.innerHTML = '<h4>' + '+' + 0.00.toFixed(2); + '</h4>';
     }
-    else if (result === NaN && result === undefined){
+    else if (isNaN(result ) || result === undefined) {
+        let nanIndex = posNums.indexOf(NaN)
+        posNums.splice(nanIndex, 1)
         return;
     }
- 
+
 
     input.value = "";
-}
-
-for (let element of posNums) {
-    console.log(element);
 }
